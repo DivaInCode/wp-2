40f0ed6eda73738a889a88f2f78913b4013b0ef2
Ferjri19220305 <jriferjri.@gmail.com>
2023-09-25 13:06:02 +0700

    controller penerbit

--- project-perpus-19220987/app/Controllers/PenerbitController.php
+++ project-perpus-19220987/app/Controllers/PenerbitController.php
@@ -6,8 +6,44 @@ use App\Controllers\BaseController;
 
 class PenerbitController extends BaseController
 {
-    public function index()
+    public function cetak_convesional()
     {
-        //
+        // pencetakan fisik buku
+    }
+    public function penerbitan_digital ()
+    {
+        // merilis konten dalam format digital
+    }
+    public function Self_Publishing ()
+    {
+        // menerbitkan karya mereka sendiri melalui platform self-publishing
+    }
+    public function Distribus_melalui_Perpustakaan ()
+    {
+        // Penerbit dapat menjual atau meminjamkan karya mereka melalui perpustakaan, baik dalam bentuk fisik maupun digital
+    }
+    public function Distribusi_melalui_Toko_Buku ()
+    {
+        // Penerbit biasanya menjalin hubungan dengan toko-toko buku fisik dan online untuk menjual produk mereka
+    }
+    public function Publikasi_Ilmiah ()
+    {
+        // untuk menerbitkan jurnal ilmiah, konferensi, dan buku ilmiah
+    }
+    public function Distribusi_Berlangganan ()
+    {
+        //  menawarkan konten mereka melalui model berlangganan
+    }
+    public function Platform_Media_Sosial ()
+    {
+        //  dapat menggunakan platform media sosial untuk mempromosikan konten mereka
+    }
+    public function Penjualan_Hak_Cipta ()
+    {
+        //  dapat menjual hak cipta atau lisensi karya mereka kepada pihak lain
+    }
+    public function Publikasi_Sendiri ()
+    {
+        //   penulis atau pengarang memilih untuk menjadi penerbit mereka sendiri dan mengurus seluruh proses penerbitan
     }
 }
