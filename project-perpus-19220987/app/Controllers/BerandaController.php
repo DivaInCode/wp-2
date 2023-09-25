15dcddd5f74151b863a2b5e1ffe747458972a4c1
DivaInCode <divardian.bsi@gmail.com>
2023-09-25 12:59:23 +0700

    controller beranda

--- project-perpus-19220987/app/Controllers/BerandaController.php
+++ project-perpus-19220987/app/Controllers/BerandaController.php
@@ -8,6 +8,26 @@ class BerandaController extends BaseController
 {
     public function index()
     {
-        //
+        //Tampilan Awal
+    }
+
+    public function about()
+    {
+        //Tentang Kami
+    }
+
+    public function contact()
+    {
+        //Kontak Hubung
+    }
+
+    public function service()
+    {
+        //Layanan
+    }
+
+    public function gallery()
+    {
+        //Foto Perpus
     }
 }
