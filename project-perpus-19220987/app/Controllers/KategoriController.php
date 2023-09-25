9b4559865e9e00fb718ba48f878c2b03bfbe0899
Aditio Nurdiansyah <nurdiansyahaditio@gmail.com>
2023-09-25 12:58:26 +0700

    controller kategori

--- project-perpus-19220987/app/Controllers/KategoriController.php
+++ project-perpus-19220987/app/Controllers/KategoriController.php
@@ -8,6 +8,32 @@ class KategoriController extends BaseController
 {
     public function index()
     {
-        //
+        // menampilkan kategori
     }
+
+    public function show()
+    {
+        // menampilkan detail
+    }
+    public function create()
+    {
+        // membuat item baru
+    }
+    public function store()
+    {
+        // menyimpan data
+    }
+    public function edit()
+    {
+        // menampilan formulir
+    }
+    public function update()
+    {
+        // menyimpan perubahan
+    }
+    public function destroy()
+    {
+        // menghapus item
+    }
+    
 }
