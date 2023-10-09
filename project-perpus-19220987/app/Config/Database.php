93b5e4f7ef8b664e7c8f53f91ecb3e2ff354dff2
19220987 Diva Ardian <145885661+DivaInCode@users.noreply.github.com>
2023-10-09 10:08:50 +0700

    Update Database.php
    perubahan pada nama database yg diambil

--- project-perpus-19220987/app/Config/Database.php
+++ project-perpus-19220987/app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => 'database.sql',
+        'database'     => 'database_19220987',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
